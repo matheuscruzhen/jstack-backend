14bc998915d0d0b690958b96ed82e4454eb94feb
Matheus <matheuscruzhen@gmail.com>
2022-08-21 16:34:25 -0300

    Refatorando o método update

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -29,28 +29,23 @@ class ContactsRepository {
   }
 
   async findById(id) {
-    const [row] = await db.query("SELECT * FROM contacts WHERE id = $1", [id]);
+    const [row] = await db.query(`SELECT * FROM contacts WHERE id = $1`, [id]);
     return row;
   }
 
   async findByEmail(email) {
-    const [row] = await db.query("SELECT * FROM contacts WHERE email = $1", [
+    const [row] = await db.query(`SELECT * FROM contacts WHERE email = $1`, [
       email,
     ]);
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
-  }
-
   async create({ name, email, phone, category_id }) {
     const [row] = await db.query(
-      `INSERT INTO contacts(name, email, phone, category_id) VALUES
-        ($1, $2, $3, $4) RETURNING *
+      `
+      INSERT INTO contacts(name, email, phone, category_id)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *
       `,
       [name, email, phone, category_id]
     );
@@ -58,14 +53,23 @@ class ContactsRepository {
     return row;
   }
 
-  update(id, { name, email, phone, category_id }) {
+  async update(id, { name, email, phone, category_id }) {
+    const [row] = await db.query(
+      `
+    UPDATE contacts
+    SET name = $1, email = $2, phone = $3, category_id = $4
+    WHERE id = $5
+    RETURNING *
+    `,
+      [name, email, phone, category_id, id]
+    );
+
+    return row;
+  }
+
+  delete(id) {
     return new Promise((resolve) => {
-      const updatedContact = { id, name, email, phone, category_id };
-
-      contacts = contacts.map((contact) =>
-        contact.id === id ? updatedContact : contact
-      );
-
+      contacts = contacts.filter((contact) => contact.id !== id);
       resolve();
     });
   }
